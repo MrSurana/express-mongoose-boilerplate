559366df5373af42c38ccf1476bca1f3151f2e0c
Vaibhav Surana <mrsurana@tabdevelopers.in>
2020-11-28 18:55:52 +0530

    added morgan for request logging into console

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const indexRouter = require('./src/routes');
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
 app.use("/", indexRouter);
 
 app.listen(port, () => {
