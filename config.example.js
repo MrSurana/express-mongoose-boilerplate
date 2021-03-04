d36bb8af243d98a4ee0bc29308ed6da0aa7036fd
Vaibhav Surana <vsurana10@gmail.com>
2021-03-04 22:50:00 +0530

    moved config/index.js to config.js, removed .env file

---
+++ config.example.js
@@ -0,0 +1,7 @@
+const config = {
+    env: 'development',
+    port: 3000,
+    mongoUri: "mongodb://localhost/express_mongoose_boilerplate"
+};
+
+module.exports = config;
--- config/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.APP_PORT || 3000,
-    mongoUri: process.env.MONGO_URI
-};
-
-module.exports = config;
