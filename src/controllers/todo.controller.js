b8e48e62b8565ab1c54663af8ddc303f5e516736
Vaibhav Surana <vsurana10@gmail.com>
2021-03-04 22:55:08 +0530

    replaced class with object in controller and repository

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,100 +1,104 @@
 const express = require('express');
 const TodoRepository = require('../repositories/todo.repository');
 
-class TodoController {
+const TodoController = {};
 
-    /**
-     * List all todos
-     * @param {express.Request} req - Express router request object
-     * @param {express.Response} res - Express router response object
-     * @param {express.NextFunction} next - Express router next function
-     */
-    static async index(req, res, next) {
-        try {
-            const todos = await TodoRepository.getAll();
-            res.status(200).json(todos);
-        } catch (err) {
-            next(err);
-        }
+/**
+ * List all todos
+ * 
+ * @param {express.Request} req - Express router request object
+ * @param {express.Response} res - Express router response object
+ * @param {express.NextFunction} next - Express router next function
+ */
+TodoController.index = async (req, res, next) => {
+    try {
+        const todos = await TodoRepository.getAll();
+        res.status(200).json(todos);
+    } catch (err) {
+        next(err);
     }
+}
 
-    /**
-     * Create a new todo
-     * @param {express.Request} req - Express router request object
-     * @param {express.Response} res - Express router response object
-     * @param {express.NextFunction} next - Express router next function
-     */
-    static async create(req, res, next) {
-        const { text } = req.body;
+/**
+ * Create a new todo
+ * 
+ * @param {express.Request} req - Express router request object
+ * @param {express.Response} res - Express router response object
+ * @param {express.NextFunction} next - Express router next function
+ */
+TodoController.create = async (req, res, next) => {
+    const { text } = req.body;
 
-        if (!text) throw new Error("'text' field is required");
+    if (!text) throw new Error("'text' field is required");
 
-        try {
-            const todo = await TodoRepository.create({ text });
-            res.status(200).json(todo);
-        } catch (err) {
-            next(err);
-        }
+    try {
+        const todo = await TodoRepository.create({ text });
+        res.status(200).json(todo);
+    } catch (err) {
+        next(err);
     }
+}
 
-    /**
-     * Show todo by id
-     * @param {express.Request} req - Express router request object
-     * @param {express.Response} res - Express router response object
-     * @param {express.NextFunction} next - Express router next function
-     */
-    static async show(req, res, next) {
-        const { id } = req.params;
+/**
+ * Show todo by id
+ * 
+ * @param {express.Request} req - Express router request object
+ * @param {express.Response} res - Express router response object
+ * @param {express.NextFunction} next - Express router next function
+ */
+TodoController.show = async (req, res, next) => {
+    const { id } = req.params;
 
-        try {
-            const todo = await TodoRepository.getById(id);
-            if (!todo) throw new Error("Todo not found!");
+    try {
+        const todo = await TodoRepository.getById(id);
+        if (!todo) throw new Error("Todo not found!");
 
-            res.status(200).json(todo);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(todo);
+    } catch (err) {
+        next(err);
     }
+}
 
-    /**
-     * Update todo by id
-     * @param {express.Request} req - Express router request object
-     * @param {express.Response} res - Express router response object
-     * @param {express.NextFunction} next - Express router next function
-     */
-    static async update(req, res, next) {
-        const { id } = req.params;
-        const { text } = req.body;
+/**
+ * Update todo by id
+ * 
+ * @param {express.Request} req - Express router request object
+ * @param {express.Response} res - Express router response object
+ * @param {express.NextFunction} next - Express router next function
+ */
+TodoController.update = async (req, res, next) => {
+    const { id } = req.params;
+    const { text } = req.body;
 
-        if (!text) throw new Error("'text' field is required");
+    if (!text) throw new Error("'text' field is required");
 
-        try {
-            const todo = await TodoRepository.update(id, { text });
-            if (!todo) throw new Error("Todo not found!");
+    try {
+        const todo = await TodoRepository.update(id, { text });
+        if (!todo) throw new Error("Todo not found!");
 
-            res.status(200).json(todo);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(todo);
+    } catch (err) {
+        next(err);
     }
+}
 
-    /**
-     * Delete todo by id
-     * @param {express.Request} req - Express router request object
-     * @param {express.Response} res - Express router response object
-     * @param {express.NextFunction} next - Express router next function
-     */
-    static async delete(req, res, next) {
-        const { id } = req.params;
+/**
+ * Delete todo by id
+ * 
+ * @param {express.Request} req - Express router request object
+ * @param {express.Response} res - Express router response object
+ * @param {express.NextFunction} next - Express router next function
+ */
+TodoController.delete = async (req, res, next) => {
+    const { id } = req.params;
 
-        try {
-            const todo = await TodoRepository.delete(id);
-            if (!todo) throw new Error("Todo not found!");
+    try {
+        const todo = await TodoRepository.delete(id);
+        if (!todo) throw new Error("Todo not found!");
 
-            res.status(200).json(todo);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(todo);
+    } catch (err) {
+        next(err);
     }
 }
 
--- src/repositories/todo.repository.js
+++ src/repositories/todo.repository.js
@@ -1,50 +1,54 @@
 const Todo = require('../models/todo.model');
 
-class TodoRepository {
-    /**
-     * Get a list of all todos
-     */
-    static async getAll() {
-        const todos = await Todo.find();
-        return todos;
-    }
+const TodoRepository = {};
 
-    /**
-     * Create a new todo
-     * @param {{text: string}} data - Todo record data
-     */
-    static async create({ text }) {
-        const todo = await Todo.create({ text });
-        return todo;
-    }
+/**
+ * Get a list of all todos
+ */
+TodoRepository.getAll = async () => {
+    const todos = await Todo.find();
+    return todos;
+}
 
-    /**
-     * Get a todo by id
-     * @param {string} id - Todo record id
-     */
-    static async getById(id) {
-        const todo = await Todo.findById(id);
-        return todo;
-    }
+/**
+ * Create a new todo
+ * 
+ * @param {{text: string}} data - Todo record data
+ */
+TodoRepository.create = async ({ text }) => {
+    const todo = await Todo.create({ text });
+    return todo;
+}
 
-    /**
-     * Update a todo by given id
-     * @param {string} id - Todo record id
-     * @param {{text: string}} data - Todo record data
-     */
-    static async update(id, { text }) {
-        const todo = await Todo.findByIdAndUpdate(id, { text }, { new: true });
-        return todo;
-    }
+/**
+ * Get a todo by id
+ * 
+ * @param {string} id - Todo record id
+ */
+TodoRepository.getById = async (id) => {
+    const todo = await Todo.findById(id);
+    return todo;
+}
 
-    /**
-     * Delete a todo by given id
-     * @param {string} id - Todo record id
-     */
-    static async delete(id) {
-        const todo = await Todo.findByIdAndDelete(id);
-        return todo;
-    }
+/**
+ * Update a todo by given id
+ * 
+ * @param {string} id - Todo record id
+ * @param {{text: string}} data - Todo record data
+ */
+TodoRepository.update = async (id, { text }) => {
+    const todo = await Todo.findByIdAndUpdate(id, { text }, { new: true });
+    return todo;
+}
+
+/**
+ * Delete a todo by given id
+ * 
+ * @param {string} id - Todo record id
+ */
+TodoRepository.delete = async (id) => {
+    const todo = await Todo.findByIdAndDelete(id);
+    return todo;
 }
 
 module.exports = TodoRepository;
