f90f609c90a06d7d268a32cbe57b292df4528a00
Vaibhav Surana <mrsurana@tabdevelopers.in>
2021-12-05 14:03:12 +0530

    return id instead of _id in todo response

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const TodoSchema = new mongoose.Schema({
-    text: { type: String, required: true }
-}, {
-    timestamps: true
-});
+const TodoSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret, options) => {
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const Todo = mongoose.model("Todo", TodoSchema);
 module.exports = Todo;
