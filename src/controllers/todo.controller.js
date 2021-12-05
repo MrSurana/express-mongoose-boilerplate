c7fb57285fcabb7b06e01b4022bc6a05da930c7d
Vaibhav Surana <mrsurana@tabdevelopers.in>
2021-12-05 12:16:28 +0530

    removed todo.repository

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,105 +1,105 @@
-const express = require('express');
-const TodoRepository = require('../repositories/todo.repository');
+const express = require("express");
+const Todo = require("../models/todo.model");
 
 const TodoController = {};
 
 /**
  * List all todos
- * 
+ *
  * @param {express.Request} req - Express router request object
  * @param {express.Response} res - Express router response object
  * @param {express.NextFunction} next - Express router next function
  */
 TodoController.index = async (req, res, next) => {
-    try {
-        const todos = await TodoRepository.getAll();
-        res.status(200).json(todos);
-    } catch (err) {
-        next(err);
-    }
-}
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (err) {
+    next(err);
+  }
+};
 
 /**
  * Create a new todo
- * 
+ *
  * @param {express.Request} req - Express router request object
  * @param {express.Response} res - Express router response object
  * @param {express.NextFunction} next - Express router next function
  */
 TodoController.create = async (req, res, next) => {
-    const { text } = req.body;
+  const { text } = req.body;
 
-    if (!text) throw new Error("'text' field is required");
+  if (!text) throw new Error("'text' field is required");
 
-    try {
-        const todo = await TodoRepository.create({ text });
-        res.status(200).json(todo);
-    } catch (err) {
-        next(err);
-    }
-}
+  try {
+    const todo = await Todo.create({ text });
+    res.status(200).json(todo);
+  } catch (err) {
+    next(err);
+  }
+};
 
 /**
  * Show todo by id
- * 
+ *
  * @param {express.Request} req - Express router request object
  * @param {express.Response} res - Express router response object
  * @param {express.NextFunction} next - Express router next function
  */
 TodoController.show = async (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const todo = await TodoRepository.getById(id);
-        if (!todo) throw new Error("Todo not found!");
+  try {
+    const todo = await Todo.findById(id);
+    if (!todo) throw new Error("Todo not found!");
 
-        res.status(200).json(todo);
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(200).json(todo);
+  } catch (err) {
+    next(err);
+  }
+};
 
 /**
  * Update todo by id
- * 
+ *
  * @param {express.Request} req - Express router request object
  * @param {express.Response} res - Express router response object
  * @param {express.NextFunction} next - Express router next function
  */
 TodoController.update = async (req, res, next) => {
-    const { id } = req.params;
-    const { text } = req.body;
+  const { id } = req.params;
+  const { text } = req.body;
 
-    if (!text) throw new Error("'text' field is required");
+  if (!text) throw new Error("'text' field is required");
 
-    try {
-        const todo = await TodoRepository.update(id, { text });
-        if (!todo) throw new Error("Todo not found!");
+  try {
+    const todo = await Todo.findByIdAndUpdate(id, { text }, { new: true });
+    if (!todo) throw new Error("Todo not found!");
 
-        res.status(200).json(todo);
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(200).json(todo);
+  } catch (err) {
+    next(err);
+  }
+};
 
 /**
  * Delete todo by id
- * 
+ *
  * @param {express.Request} req - Express router request object
  * @param {express.Response} res - Express router response object
  * @param {express.NextFunction} next - Express router next function
  */
 TodoController.delete = async (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const todo = await TodoRepository.delete(id);
-        if (!todo) throw new Error("Todo not found!");
+  try {
+    const todo = await Todo.findByIdAndDelete(id);
+    if (!todo) throw new Error("Todo not found!");
 
-        res.status(200).json(todo);
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(200).json(todo);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = TodoController;
