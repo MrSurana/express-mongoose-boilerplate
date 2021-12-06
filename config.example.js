2c4e4f5ca36d81dcd5c3e39a112c2ff878b95e69
Vaibhav Surana <mrsurana@tabdevelopers.in>
2021-12-06 09:12:42 +0530

    added NODE_ENV for env in config

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 const config = {
-    env: 'development',
-    port: 3000,
-    mongoUri: "mongodb://localhost/express_mongoose_boilerplate"
+  env: process.env.NODE_ENV || "development",
+  port: 3000,
+  mongoUri: "mongodb://localhost/express_mongoose_boilerplate",
 };
 
 module.exports = config;
